9774154978da0d1b1d5d8586a202ca66a997b059
jdfincher <jessefincher@protonmail.com>
2025-10-15 11:52:24 -0500

    setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	rabbitURI := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(rabbitURI)
+	if err != nil {
+		fmt.Println("Something went wrong establishing amqp connection", err)
+		os.Exit(1)
+	}
+	defer connection.Close()
+	fmt.Println("Rabbitmq connection was successfull...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nProgram terminating...")
+	os.Exit(0)
 }
